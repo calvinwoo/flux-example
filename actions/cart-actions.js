eaa564718619521022ffb710257270e0b07ef3c2
Calvin Woo <calvinwoo32@gmail.com>
2015-07-26 03:26:09 -0700

    Flux action comments.

--- actions/cart-actions.js
+++ actions/cart-actions.js
@@ -4,6 +4,7 @@ import cartApi from '../fake-api/cart'
 const CART_UPDATE = 'cart_update'
 
 export default {
+  // Retrieve cart info from the API, and set Flux up with the initial data.
   retrieveInitial() {
     cartApi.get().then(cart => {
       appDispatcher.dispatch({
@@ -12,6 +13,9 @@ export default {
       })
     })
   },
+  // Send an API request with the new data.
+  // At the same time, optimistically update Flux, assuming the request is valid.
+  // This gets new information to the components more quickly.
   update(cart) {
     appDispatcher.dispatch({
       actionType: CART_UPDATE,
