c8aeff5c0c6b35964e661a66883e9b56c170e99d
Calvin Woo <calvinwoo32@gmail.com>
2015-07-26 02:23:20 -0700

    Commented fake api.

--- fake-api/cart.js
+++ fake-api/cart.js
@@ -1,6 +1,12 @@
+/**
+ * A fake API that uses browser localStorage.
+ *
+ * Pretend that this module is actually making AJAX requests to some API with a shopping cart list.
+ */
 export default {
 	get() {
 		return new Promise((resolve, reject) => {
+			// If the "cart" is initially empty, populate it with some good films.
 			if (!localStorage['fakeCart']) {
 				localStorage['fakeCart'] = JSON.stringify([
 					{title: 'Chinatown'},
